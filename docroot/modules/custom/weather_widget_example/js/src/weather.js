1113e67a2d3ab13bac794ed99ddffd8bf52e72c0
Brian Reese <brian.reese@acquia.com>
2018-08-10 10:01:44 -0400

    DEMO: Implementation of Drupal theme functions.

--- docroot/modules/custom/weather_widget_example/js/src/weather.js
+++ docroot/modules/custom/weather_widget_example/js/src/weather.js
@@ -22,4 +22,28 @@ import WeatherWidget, { DEFAULTWIDGETSELECTOR } from '../modules/weatherWidget';
     }
   }
 
+  /**
+   * Define Theme functions for the weather widget.
+   *
+   * Clone original render methods before overriding the WeatherWidget with our
+   * own theme function. This will allow other modules and themes to provide
+   * their own theme implementations.
+   */
+  const WeatherWidgetRenderMethods = Object.assign({}, {
+    renderHeader: WeatherWidget.renderHeader,
+    renderWeatherCard: WeatherWidget.renderWeatherCard,
+  });
+
+  Drupal.theme.weatherCardHeader = WeatherWidgetRenderMethods.renderHeader;
+  Drupal.theme.weatherCard = WeatherWidgetRenderMethods.renderWeatherCard;
+
+  /**
+   * Override WeatherWidget's render methods to leverage drupal's theme system.
+   *
+   * This will allow other modules and themes to provide their own
+   * implementations of Drupal.theme.weatherCard.
+   */
+  WeatherWidget.renderWeatherCard = (...args) => Drupal.theme('weatherCard', ...args);
+  WeatherWidget.renderHeader = (...args) => Drupal.theme('weatherCardHeader', ...args);
+
 })(Drupal);
