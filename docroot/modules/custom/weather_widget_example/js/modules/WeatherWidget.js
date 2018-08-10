35f7051990fd45555666dec498f4bcaa4aa00807
Brian Reese <brian.reese@acquia.com>
2018-08-10 10:00:19 -0400

    DEMO: Render the weather widget.

--- docroot/modules/custom/weather_widget_example/js/modules/WeatherWidget.js
+++ docroot/modules/custom/weather_widget_example/js/modules/WeatherWidget.js
@@ -34,7 +34,7 @@ class WeatherWidget {
    *   A weather widget instance.
    */
   static create(widget) {
-    const instance = new this(widget);
+    const instance = new WeatherWidget(widget);
 
     // Render initial output.
     // weatherWidget::refresh() is an async method that returns a promise, but
@@ -50,7 +50,43 @@ class WeatherWidget {
    *   Weather data returned from the weather service.
    */
   render(data) {
+    console.log(data);
+    if (!data.channel) {
+      // @todo: api response was not what we expected.
+      return;
+    }
+
+    let container = document.createElement('div');
+    container.classList.add('weather-widget--inner');
+
+    container.innerHTML = this.constructor.renderHeader(data.channel.location);
+    const forecastData = ((data.channel.item || {}).forecast || []);
+    forecastData.forEach(day => {
+      container.innerHTML += this.constructor.renderWeatherCard(day.day, day.code, day.low, day.high);
+    })
+    console.log(forecastData);
+
+
+
     // @todo: render the widget to the dom.
+    this.widget.innerHTML = '';
+    this.widget.append(container);
+  }
+
+  static renderHeader(location) {
+    return `<h2>${location.city}, ${location.region}</h2>`;
+  }
+
+  static renderWeatherCard(date, weatherCode, low, high) {
+    let iconClass = WeatherIcons.getIconClass(weatherCode);
+    let conditions = WeatherIcons.getDescription(weatherCode);
+    return `<div>
+      <i class="wi ${iconClass}"></i>
+      <h4>${date}</h4>
+      <span>${conditions}</span>
+      <span>High: ${high}</span>
+      <span>Low: ${low}</span>
+    </div>`
   }
 
   /**
@@ -58,7 +94,8 @@ class WeatherWidget {
    */
   async refresh() {
     // @todo: fetch weather data from the weather service, then render it.
-    await weatherService.getWeatherData(10027);
+    const data = await weatherService.getWeatherData(10027);
+    this.render(data);
   }
 
 }
